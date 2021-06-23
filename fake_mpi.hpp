4062744cf46b32104a11c3014ffe8af4205ef7f7
Carl pearson <cwpears@sandia.gov>
2021-06-23 14:06:44 -0600

    argument skeleton

---
+++ fake_mpi.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef REAL_MPI
+struct MPI_Datatype{};
+struct MPI_Comm{};
+struct MPI_Request{};
+struct MPI_Status{};
+int MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest, int tag,
+              MPI_Comm comm, MPI_Request *request) {return 0;}
+int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source,
+int tag, MPI_Comm comm, MPI_Request * request) {return 0;}
+int MPI_Wait(MPI_Request * request, MPI_Status * status) {return 0;}
+int MPI_Init(int *argc, char ***argv) {return 0;}
+int MPI_Finalize() {return 0;}
+MPI_Comm MPI_COMM_WORLD;
+#else
+#include <mpi.h>
+#endif
+
+struct IrecvArgs{};
+struct IsendArgs{};
+struct WaitArgs{};
--- main.cpp
+++ main.cpp
@@ -3,139 +3,250 @@
 #include <iostream>
 #include <string>
 
-class Operation {
+#ifndef CUDA_DEFINES
+typedef int cudaStream_t;
+typedef int cudaError_t;
+cudaError_t cudaStreamCreate(cudaStream_t *stream) {*stream=1; return 0;}
+#else 
+#include <cuda_runtime.h>
+#endif
+
+#include "fake_mpi.hpp"
+
+class Operation
+{
 public:
     virtual void run() {}
 
     std::set<Operation *> succs;
     std::set<Operation *> preds;
 
-
     // do op after this
-    Operation *then(Operation *op) {
+    Operation *then(Operation *op)
+    {
         succs.insert(op);
         op->preds.insert(this);
         return op;
     }
 
-    virtual ~Operation() {};
-    virtual std::string name() {return "<anon>";}
-};
+    virtual ~Operation(){};
+    virtual std::string name() { return "<anon>"; }
 
-class Start : public Operation {
-    public:
-    std::string name() override {return "Start"; }
-};
+    /*this operation replaces itself with the provided operation*/
+    void replace_with(Operation *op)
+    {
+        // replace self in successors' preds
+        for (Operation *succ : succs)
+        {
+            succ->preds.erase(this);
+            succ->preds.insert(op);
+        }
 
-class End : public Operation {
-    public:
-    std::string name() override {return "End"; }
-};
+        // replace self in predecessors succs
+        for (Operation *pred : preds)
+        {
+            pred->succs.erase(this);
+            pred->succs.insert(op);
+        }
 
-class SpMV : public Operation {
-public:
-std::string name_;
-SpMV(const std::string name) : name_(name) {}
-std::string name() override {return name_; }
-};
+        // replace op's preds and succs with mine
+        op->succs = succs;
+        op->preds = preds;
 
-class VectorAdd : public Operation {
-public:
-std::string name_;
-VectorAdd(const std::string name) : name_(name) {}
-std::string name() override {return name_; }
-};
-
-class Scatter : public Operation {
-public:
-std::string name() override {return "scatter"; }
-};
-
-class PostRecv : public Operation {
-public:
-std::string name() override {return "PostRecv"; }
-};
-
-class WaitRecv : public Operation {
-public:
-std::string name() override {return "WaitRecv"; }
-};
-
-class PostSend : public Operation {
-public:
-std::string name() override {return "PostSend"; }
-};
-
-class WaitSend : public Operation {
-public:
-std::string name() override {return "WaitSend"; }
-};
-
-
-
-class Schedule {
-public:
-std::set<Operation *> remaining; // possible next operations
-std::vector<Operation *> order;
-
-void run() {
-    for (Operation *op : order) {
-        op->run();
+        succs.clear();
+        preds.clear();
     }
-}
 };
 
+class Start : public Operation
+{
+public:
+    std::string name() override { return "Start"; }
+};
 
+class End : public Operation
+{
+public:
+    std::string name() override { return "End"; }
+};
 
-std::vector<Schedule> make_schedules(Operation *start) {
+class SpMV : public Operation
+{
+public:
+    struct Args {
+        float *y;
+        int yN;
+        float *x;
+        int xN;
+        int *rowPtr;
+        int *colInd;
+        float *colVal;
+        int nRow;
+    };
+
+    std::string name_;
+    Args args_;
+    cudaStream_t stream_;
+    SpMV(const std::string name, Args args, cudaStream_t stream) : name_(name), args_(args), stream_(stream) {}
+    std::string name() override { return name_ + "(" + std::to_string(uintptr_t(stream_)) + ")"; }
+};
+
+/* y[i] += a[i]
+*/
+class VectorAdd : public Operation
+{
+public:
+struct Args {
+    float *y;
+    float *a;
+    int n;
+};
+    std::string name_;
+    cudaStream_t stream_;
+    Args args_;
+    VectorAdd(const std::string name, Args args, cudaStream_t stream) : name_(name), args_(args), stream_(stream) {}
+    std::string name() override { return name_ + "(" + std::to_string(uintptr_t(stream_)) + ")"; }
+};
+
+/* 
+   dst[i] = src[idx[i]]
+   dst[0..dstN]
+   src[0..srcN]
+   idx[0..srcN]
+*/
+class Scatter : public Operation
+{
+public:
+    struct Args {
+        float *dst;
+        int dstN;
+        float *src;
+        int srcN;
+        int *idx;
+    };
+    Args args_;
+    cudaStream_t stream_;
+    Scatter(Args args, cudaStream_t stream) : args_(args), stream_(stream) {}
+    std::string name() override { return "Scatter(" + std::to_string(uintptr_t(stream_)) + ")"; }
+};
+
+class StreamSync : public Operation
+{
+public:
+    cudaStream_t stream_;
+    StreamSync(cudaStream_t stream) : stream_(stream) {}
+    std::string name() override { return "StreamSync(" + std::to_string(uintptr_t(stream_)) + ")"; }
+};
+
+class PostRecv : public Operation
+{
+public:
+    struct Args {
+        std::vector<IrecvArgs> recvs; 
+    };
+    Args args_;
+    PostRecv(Args args) : args_(args) {}
+    std::string name() override { return "PostRecv"; }
+};
+
+class WaitRecv : public Operation
+{
+public:
+    typedef PostRecv::Args Args;
+    Args args_;
+    WaitRecv(Args args) : args_(args) {}
+    std::string name() override { return "WaitRecv"; }
+};
+
+class PostSend : public Operation
+{
+public:
+    struct Args {
+        std::vector<IsendArgs> sends; 
+    };
+    Args args_;
+    PostSend(Args args) : args_(args) {}
+    std::string name() override { return "PostSend"; }
+};
+
+class WaitSend : public Operation
+{
+public:
+    typedef PostSend::Args Args;
+    Args args_;
+    WaitSend(Args args) : args_(args) {}
+    std::string name() override { return "WaitSend"; }
+};
+
+class Schedule
+{
+public:
+    std::set<Operation *> remaining; // possible next operations
+    std::vector<Operation *> order;
+
+    void run()
+    {
+        for (Operation *op : order)
+        {
+            op->run();
+        }
+    }
+};
+
+std::vector<Schedule> make_schedules(Operation *start)
+{
     std::vector<Schedule> currs; // input generation
     std::vector<Schedule> ret;
-    
+
     {
         Schedule s;
         s.remaining.insert(start);
         currs.push_back(s);
     }
 
-
-    while(!currs.empty()) {
+    while (!currs.empty())
+    {
         // all possible schedules derived from current schedules
         // followed by legal next operations
         std::vector<Schedule> nexts;
-        
-        for (Schedule &curr : currs) {
+
+        for (Schedule &curr : currs)
+        {
             // if no more allowed operations, the schedule
             // must be complete
-            if (curr.remaining.empty()) {
+            if (curr.remaining.empty())
+            {
                 ret.push_back(curr);
-            } else {
-                // create schedules which are `curr` followed
-                // by each legal next operation
-                for (Operation *nextOp : curr.remaining) {
+            }
+            else
+            {
+                // create schedules which are `curr` followed by each legal next operation
+                for (Operation *nextOp : curr.remaining)
+                {
 
                     // check if nextOp's preds are all done
                     bool allDone = true;
-                    for (Operation *check : nextOp->preds) {
-                        if (curr.order.end() == std::find(curr.order.begin(), curr.order.end(), check)) {
+                    for (Operation *check : nextOp->preds)
+                    {
+                        if (curr.order.end() == std::find(curr.order.begin(), curr.order.end(), check))
+                        {
                             allDone = false;
                             break;
                         }
                     }
 
-                    // if all preds are done, nextOp can be
-                    // next
-                    if (allDone) {
+                    // if all preds are done, nextOp can be next
+                    if (allDone)
+                    {
                         Schedule next = curr;
                         next.remaining.erase(nextOp);
                         next.order.push_back(nextOp);
-                        for (Operation *succ : nextOp->succs) {
+                        for (Operation *succ : nextOp->succs)
+                        {
                             next.remaining.insert(succ);
                         }
                         nexts.push_back(next);
                     }
-
-
-
                 }
             }
         }
@@ -145,27 +256,57 @@ std::vector<Schedule> make_schedules(Operation *start) {
     return ret;
 };
 
+int main(int argc, char **argv)
+{
 
+    MPI_Init(&argc, &argv);
 
-int main(void) {
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
 
     Start *start = new Start();
-    Scatter *scatter = new Scatter();
-    SpMV *yl = new SpMV("yl");
-    SpMV *yr = new SpMV("yr");
-    PostSend *postSend = new PostSend();
-    WaitSend *waitSend = new WaitSend();
-    PostRecv *postRecv = new PostRecv();
-    WaitRecv *waitRecv = new WaitRecv();
-    VectorAdd *y = new VectorAdd("y");
-    End *end = new End();
-    
 
-    // immidiately recv, local spmv, or scatter
+    Scatter *scatter;
+    {
+        Scatter::Args args;
+        scatter = new Scatter(args, stream);
+    }
+
+    SpMV *yl, *yr;
+    {
+        SpMV::Args rArgs, lArgs;
+        yl = new SpMV("yl", lArgs, stream);
+        yr = new SpMV("yr", rArgs, stream);
+    }
+
+    PostSend *postSend;
+    WaitSend *waitSend;
+    {
+        PostSend::Args args;
+        postSend = new PostSend(args);
+        waitSend = new WaitSend(args);
+    }    
+    
+    PostRecv *postRecv;
+    WaitRecv *waitRecv;
+    {
+        PostRecv::Args args;
+        postRecv = new PostRecv(args);
+        waitRecv = new WaitRecv(args);
+    }
+    VectorAdd *y;
+    {
+        VectorAdd::Args args;
+        y = new VectorAdd("y", args, stream);
+    }
+    StreamSync *streamSync = new StreamSync(stream);
+    End *end = new End();
+
+    // immediately recv, local spmv, or scatter
     start->then(yl);
     start->then(postRecv);
     start->then(scatter)->then(postSend);
-    
+
     // remote matrix after recv
     waitRecv->then(yr)->then(end);
 
@@ -174,7 +315,7 @@ int main(void) {
     yr->then(y);
 
     // end once add and send is done
-    y->then(end);
+    y->then(streamSync)->then(end);
     waitSend->then(end);
 
     // initiate sends and recvs before waiting for either
@@ -186,15 +327,19 @@ int main(void) {
     std::vector<Schedule> schedules = make_schedules(start);
 
     std::cerr << schedules.size() << " schedules:\n";
-    for (Schedule &s : schedules) {
-        for (Operation *op : s.order) {
+    for (Schedule &s : schedules)
+    {
+        for (Operation *op : s.order)
+        {
             std::cerr << op->name() << ", ";
         }
         std::cerr << "\n";
     }
 
-    for (Schedule &sched : schedules) {
+    for (Schedule &sched : schedules)
+    {
         sched.run();
     }
 
+    MPI_Finalize();
 }

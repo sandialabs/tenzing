36d3addedf47a3c6fecac7bc5321774f6631585a
Carl William Pearson <cwpears@sandia.gov>
2021-12-10 15:52:51 -0700

    simplify Event and Stream json

--- src/platform.cpp
+++ src/platform.cpp
@@ -1,19 +1,23 @@
 #include "sched/platform.hpp"
 
 void to_json(nlohmann::json& j, const Stream &s) {
-    j = nlohmann::json{{"id", s.id_}};
+    // j = nlohmann::json{{"id", s.id_}};
+    j = nlohmann::json(s.id_);
 }
 
 void from_json(const nlohmann::json& j, Stream &s) {
-    j.at("id").get_to(s.id_);
+    // j.at("id").get_to(s.id_);
+    j.get_to(s.id_);
 }
 
-void to_json(nlohmann::json& j, const Event &s) {
-    j = nlohmann::json{{"id", s.id_}};
+void to_json(nlohmann::json& j, const Event &e) {
+    // j = nlohmann::json{{"id", e.id_}};
+    j = nlohmann::json(e.id_);
 }
 
-void from_json(const nlohmann::json& j, Event &s) {
-    j.at("id").get_to(s.id_);
+void from_json(const nlohmann::json& j, Event &e) {
+    // j.at("id").get_to(e.id_);
+    j.get_to(e.id_);
 }
 
 

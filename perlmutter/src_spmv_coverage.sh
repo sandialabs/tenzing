72733e4b86cd980d3146fcb5d2db057c6580e968
Carl Pearson <cwpears@sandia.gov>
2021-12-22 07:50:36 -0800

    perlmutter scripts

--- perlmutter/src_spmv_coverage.sh
+++ perlmutter/src_spmv_coverage.sh
@@ -2,22 +2,22 @@
 #SBATCH -A m3918_g
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 4:00:00
+#SBATCH -t 1:00:00
 #SBATCH -n 4
 #SBATCH --ntasks-per-node=4
 #SBATCH -c 16
 #SBATCH --gpus-per-task=1
-#SBATCH -e spmv_whole_tree.e%j
-#SBATCH -o spmv_whole_tree.o%j
+#SBATCH -e src_spmv_coverage.e%j
+#SBATCH -o src_spmv_coverage.o%j
 #SBATCH --signal=SIGABRT@10
 
 DIR=/global/homes/p/pearson/repos/sched
-EXE=$DIR/build-perlmutter/src_spmv/platform-mcts-random
+EXE=$DIR/build-perlmutter/src_spmv/platform-mcts-coverage
 
 source $DIR/load-env.sh
 
 export SLURM_CPU_BIND="cores"
 
 srun -G 4 $EXE -i 0 \
-| tee $DIR/perlmutter/spmv_whole_tree.csv
+| tee $DIR/perlmutter/src_spmv_coverage.csv
 

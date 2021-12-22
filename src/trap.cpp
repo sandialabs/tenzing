2972f44b67354a6910621095b2093f792c4dc451
Carl Pearson <cwpears@sandia.gov>
2021-12-22 07:48:05 -0800

    catch SIGABRT

--- src/trap.cpp
+++ src/trap.cpp
@@ -11,11 +11,20 @@ void int_caller(int signal) {
     exit(1);
 }
 
+void abrt_caller(int signal) {
+    std::cerr << "CAUGHT " << signal << "\n";
+    unregister_handler();
+    global_handler(signal);
+    exit(1);
+}
+
 void register_handler(std::function<void(int)> func) {
     global_handler = func;
     signal(SIGINT, int_caller);
+    signal(SIGABRT, abrt_caller);
 }
 
 void unregister_handler() {
+    signal(SIGABRT, SIG_DFL);
     signal(SIGINT, SIG_DFL);
 }

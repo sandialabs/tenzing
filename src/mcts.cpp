0d0b98fe529e99e56531240625351e56f03cec17
Carl Pearson <cwpears@sandia.gov>
2021-12-22 09:40:52 -0800

    remove old mpi_bcast code

--- src/mcts.cpp
+++ src/mcts.cpp
@@ -7,80 +7,6 @@
 
 namespace mcts {
 
-#if 0
-void mpi_bcast(std::vector<std::shared_ptr<CpuNode>> &order, MPI_Comm comm) {
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    // bcast number of operations in order
-    int orderSize = order.size();
-    MPI_Bcast(&orderSize, 1, MPI_INT, 0, comm);
-
-    // bcast length of name of each operation
-    std::vector<int> nameLengths;
-    if (0 == rank) {
-        for (const auto &op : order) {
-            nameLengths.push_back(op->name().size());
-        }
-    } else {
-        nameLengths.resize(orderSize);
-    }
-    MPI_Bcast(nameLengths.data(), nameLengths.size(), MPI_INT, 0, comm);
-
-    // bcast names
-    size_t totalLength = 0;
-    for (auto l : nameLengths) {
-        totalLength += l;
-    }
-    std::vector<char> allNames;
-    if (0 == rank) {
-        for (const auto &op : order) {
-            for (auto c : op->name()) {
-                allNames.push_back(c);
-            }
-        }
-    } else {
-        allNames.resize(totalLength);
-    }
-    MPI_Bcast(allNames.data(), allNames.size(), MPI_CHAR, 0, comm);
-
-    // break into strings
-    std::vector<std::string> names;
-    size_t off = 0;
-    for (auto l : nameLengths) {
-        names.push_back(std::string(&allNames[off], l));
-        off += l;
-    }
-
-    // find corresponding op in order if recieving
-    if (0 != rank) {
-        std::vector<size_t> pos;
-        for (const std::string &name : names) {
-
-            bool found = false;
-            for (size_t oi = 0; oi < order.size(); ++oi) {
-                if (order[oi]->name() == name) {
-                    pos.push_back(oi);
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                THROW_RUNTIME("couldn't find op for name " << name);
-            }
-        }
-
-        // reorder operations
-        std::vector<std::shared_ptr<CpuNode>> perm;
-        for (size_t oi : pos) {
-            perm.push_back(order[oi]);
-        }
-        order = perm;
-    }
-}
-#endif
-
 /* broadcast `order` from rank 0 to the other ranks
 */
 std::vector<std::shared_ptr<BoundOp>> mpi_bcast(
@@ -126,5 +52,26 @@ std::vector<std::shared_ptr<BoundOp>> mpi_bcast(
     }
 
 }
+
+void Result::dump_csv() const {
+
+    const std::string delim("|");
+
+    for (size_t i = 0; i < simResults.size(); ++i) {
+        const auto &simres = simResults[i];
+        std::cout << i;
+        std::cout << delim << simres.benchResult.pct01;
+        std::cout << delim << simres.benchResult.pct10;
+        std::cout << delim << simres.benchResult.pct50;
+        std::cout << delim << simres.benchResult.pct90;
+        std::cout << delim << simres.benchResult.pct99;
+        std::cout << delim << simres.benchResult.stddev;
+        for (const auto &op : simres.path) {
+            std::cout << "|" << op->json();
+        }
+        std::cout << "\n"; 
+    }
+}
+
 
 } // namespace mcts

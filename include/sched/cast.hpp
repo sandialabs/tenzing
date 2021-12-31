6f1c8db71de335ef573e50d049bdd33a2c4ff0f2
Carl Pearson <cwpears@sandia.gov>
2021-12-31 09:22:33 -0800

    add necessary includes

--- include/sched/cast.hpp
+++ include/sched/cast.hpp
@@ -1,25 +1,27 @@
 #pragma once
 
+#include <memory>
+#include <sstream>
+
 namespace sched {
 
 // dynamic_pointer_cast U to T, or throw
-template <typename T, typename U> std::shared_ptr<T> cast_or_throw(
-    const std::shared_ptr<U> &u,
-    const char *file,
-    int line
-) {
-        auto t = std::dynamic_pointer_cast<T>(u);
-        if (!t) {
-            std::stringstream ss;
-            ss << file << ":" << line << ": " << "failed pointer cast" << "\n";
-            throw std::runtime_error(ss.str());
-        }
-        return t;
+template <typename T, typename U>
+std::shared_ptr<T> cast_or_throw(const std::shared_ptr<U> &u, const char *file, int line) {
+  auto t = std::dynamic_pointer_cast<T>(u);
+  if (!t) {
+    std::stringstream ss;
+    ss << file << ":" << line << ": "
+       << "failed pointer cast"
+       << "\n";
+    throw std::runtime_error(ss.str());
+  }
+  return t;
 }
 } // namespace sched
 
 #define SCHED_CAST_OR_THROW(T, x) sched::cast_or_throw<T>(x, __FILE__, __LINE__)
 
 template <typename T, typename U> bool isa(const std::shared_ptr<U> &ptr) {
-    return bool(std::dynamic_pointer_cast<T>(ptr));
+  return bool(std::dynamic_pointer_cast<T>(ptr));
 }

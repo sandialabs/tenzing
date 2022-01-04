aab6fb7305ea4789dc00b121176deee5da821d46
Carl Pearson <cwpears@sandia.gov>
2022-01-04 11:14:33 -0800

    halo exchange MCTS

--- src_mcts_halo/halo_min_time.cu
+++ src_mcts_halo/halo_min_time.cu
@@ -1,10 +1,10 @@
-#include "sched/mcts_strategy_min_time.hpp"
+#include "sched/mcts_strategy_fast_min.hpp"
 
 #include "run_strategy.hpp"
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
-    int err = doit<mcts::MinTime>(argc, argv);
+    int err = doit<mcts::FastMin>(argc, argv);
     MPI_Finalize();
 }
 
